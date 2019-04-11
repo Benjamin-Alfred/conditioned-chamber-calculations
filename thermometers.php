d8cd77828c2f3b5a62cbcdc4504bd9f810f79129
Thomas M. Nyongesa <mapesa@gmail.com>
2019-04-11 14:10:32 +0300

    Update CC and Therm to user view/menu.php

--- thermometers.php
+++ thermometers.php
@@ -29,26 +29,7 @@ get_header();
     <div id="primary" class="content-area" style="padding-top: 0;">
         <main id="main" class="site-main" role="main">
             <div class="row d-print-none">
-                <div class="dropdown">
-                    <button class="btn btn-outline-light" type="button" id="dropdownMenuButton"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="oi" data-glyph="menu" title="menu" aria-hidden="true" style="color: #000;"></span>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <?php if(hasRole('USER_ADMIN')){ ?>
-                            <button class="dropdown-item" id="dashboard-menu">Dashboard</button>
-                            <div class="dropdown-divider"></div>
-                        <?php } ?>
-                        <button class="dropdown-item" id="conditioned-chambers-menu">Conditioned Chambers</button>
-                        <button class="dropdown-item" id="service-requests-menu">Service Requests</button>
-                        <a class="dropdown-item" href="/centrifuges/">Centrifuges</a>
-                        <a class="dropdown-item" href="/thermometers/">Thermometers</a>
-                        <?php if(hasRole('USER_ADMIN')){ ?>
-                            <div class="dropdown-divider"></div>
-                            <button class="dropdown-item" id="client-contacts-menu">Client Contacts</button>
-                        <?php } ?>
-                    </div>
-                </div>
+                <?php include('views/menu.php'); ?>
                 <div class="col">
                     <h4>Thermometers</h4>
                 </div>
